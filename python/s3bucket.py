f215eb85e562f858ad5997ec76c8e4e75a387d78
Adam Snetiker <ajsnetiker@gmail.com>
2021-02-26 18:40:08 -0800

    Corrected errors from pylint

--- python/s3bucket.py
+++ python/s3bucket.py
@@ -1,18 +1,18 @@
-'''This script creates an S3 bucket using the Boto3 library.'''
+'This script creates an S3 bucket using the Boto3 library'
 import sys
 import boto3
 
 def main():
-    '''Main function executed to create the bucket'''
+    'Main function executed to create the bucket'
     create_s3bucket(bucket_name)
 
 def create_s3bucket(bucket_name):
+    'This function sets up S3 bucket creation with the name, location, and ACL'
     s3_bucket = boto3.client(
         's3',
     )
 
     s3_bucket.create_bucket()(
-        '''Bucket creation function that setss the name, location, and access'''
         Bucket = bucket_name,
         CreateBucketConfiguration = {
             'LocationConstraint': 'us-west-1'
